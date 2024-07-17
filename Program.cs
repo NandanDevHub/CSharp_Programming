f94c895a4753cb71d7b96f6ebbe304784ef6dac9
nandan <parmar.nandan189@gmail.com>
2024-07-17 22:33:00 +0530

    Conditional Statements

--- Program.cs
+++ Program.cs
@@ -1,4 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// New Code
-// nEw Codedsssdss
+﻿float baseSalary = 50000;
+float hra = 0.5F;
+
+float netSalary = baseSalary - (baseSalary * hra);
+
+Console.WriteLine("The Base Salary is: " + baseSalary);
+Console.WriteLine("The Salary credited in account will be: " + netSalary);
+
+bool isAuthenticated = true;
+
+string result = isAuthenticated ? "logged in" : "logged out";
+Console.WriteLine(result);
+
+switch (result)
+{
+    case ("logged in"):
+        Console.WriteLine("You are in control");
+        break;
+
+    case ("logged out"):
+        Console.WriteLine("You do not have access");
+        break;
+
+    default:
+        Console.WriteLine("Denied");
+        break;
+}
+
+string[] names = new string[5] { "Shikhar Dhawan", "Rohit Sharma", "Virat Kohli", "Suresh Raina", "MS Dhoni" };
+
+//
